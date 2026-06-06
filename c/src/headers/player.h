d22f1cdf1c10b37f7b72ad8f5ffd875bd7be1a40
Cashion <danielacashion@gmail.com>
2026-06-06 13:35:01 -0500

    adding missed headers

---
+++ c/src/headers/player.h
@@ -0,0 +1,25 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+#include "map.h"
+#include <math.h>
+#include "defs.h"
+
+
+typedef struct {
+  float x;
+  float y;
+  float width;
+  float height;
+  int turnDirection; //-1 left, 1 right
+  int walkDirection; //-1 back, 1 forward
+  float rotationAngle;
+  float walkSpeed;
+  float turnSpeed;
+} player_t;
+
+extern player_t player;
+void movePlayer(float deltaTime);
+void renderPlayer(void);
+
+
+#endif
---
+++ c/src/headers/ray.h
@@ -0,0 +1,24 @@
+#ifndef RAY_H
+#define RAY_H
+#include <stdbool.h>
+#include "defs.h"
+#include "player.h"
+
+typedef struct {
+  float rayAngle;
+  float wallhitx;
+  float wallhity;
+  bool wasHitVert;
+  float distance;
+  int wallColor;
+} ray_t;
+
+extern ray_t rays[NUM_RAYS];
+
+void castAllRays(void);
+void normalizeAngle(float *rayAngle);
+float distanceBetweenPoints(float x1, float y1, float x2, float y2);
+
+void renderRays(void);
+
+#endif
